dec14a60eaea4491bf1c6364cdb8a98ccd186140
Caleb Jones <self@calebjones.net>
2016-06-02 21:56:38 -0400

    Add a test program to parse Pecan source files
    Add a test program that currently won't parse

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,25 @@ pub mod lexer;
 pub mod parser;
 
 use lexer::Lexer;
+use std::fs::File;
+use std::io::Read;
+use std::env;
 
 fn main() {
-    println!("Hello, World!");
+    let args: Vec<_> = env::args().collect();
+    if args.len() < 2 {
+        println!("Pass a filename as argument");
+        return;
+    }
+    let file = File::open(&args[1]);
+    let mut buf = String::new();
+    if let Err(e) = file.and_then(|mut f| f.read_to_string(&mut buf)) {
+        println!("Error reading file: {}", e);
+        return;
+    }
+    let lexer = Lexer::new(buf.as_str());
+    let parse_result = parser::parse_File(lexer);
+    println!("{:?}", parse_result);
 }
 
 #[test]
